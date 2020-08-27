5b0d9454fbb2aa436ca0de7863f5ad9b9dcddfbb
SeoWS <seows2@naver.com>
2020-08-27 20:11:12 +0900

    about페이지에 footer 제거

--- js/photo.js
+++ js/photo.js
@@ -8,13 +8,35 @@ const CLASS = {
 };
 
 const initalizeMasonry = () => {
-  const elem = document.querySelector(CLASS.GRID);
-  new Masonry(elem, {
+  const grid = document.querySelector(CLASS.GRID);
+  new Masonry(grid, {
     itemSelector: CLASS.GRID_ITEM,
     columnWidth: CLASS.GRID_SIZER,
     gutter: CLASS.GUTTER_SIZER,
   });
 };
+const initObserver = (io) => {
+  const placeholders = document.querySelectorAll(".placeholder");
+  placeholders.forEach((placeholder) => io.observe(placeholder));
+};
+
+const initScrollAction = () => {
+  const io = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("placeholder--scale");
+        }
+      });
+    },
+    { rootMargin: "0px 0px 200px 0px" }
+  );
+  initObserver(io);
+};
+
 if (document.querySelector(".photo_html")) {
-  initalizeMasonry();
+  window.onload = () => {
+    initalizeMasonry();
+    initScrollAction();
+  };
 }
