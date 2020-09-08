0a59237ba179d04c86cdfe98e865d81dafbbb63d
SeoWS <seows2@naver.com>
2020-09-08 18:14:51 +0900

    Updates

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: `/assets`,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "main.css" }), new Dotenv()],
+};
+
+module.exports = config;
